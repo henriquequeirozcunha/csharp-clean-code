ae4f92e12331292f30fa0004ce8de3434e6f6f21
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-12-31 15:33:20 -0300

    feat: add employeeId on LeaveAllocation

---
+++ src/Infrastructure/Persistence/Migrations/20221231183024_AddEmployeeIdLeaveAllocation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeIdLeaveAllocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeId",
+                table: "LeaveAllocations",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "LeaveAllocations");
+        }
+    }
+}
